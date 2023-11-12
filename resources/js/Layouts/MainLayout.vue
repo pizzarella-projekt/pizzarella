04816675a0f21e290572b9d893bdaf033c15de44
Tobiasz-Sobczyk <tobiasz.sobczyk@student.po.edu.pl>
2023-11-12 19:45:04 +0100

    dodanie layoutu strony

---
+++ resources/js/Layouts/MainLayout.vue
@@ -0,0 +1,29 @@
+<script setup>
+import ApplicationLogo from '@/Components/ApplicationLogo.vue';
+import { Link } from '@inertiajs/vue3';
+</script>
+
+<template>
+    <div class="min-h-screen flex flex-col items-center bg-gray-100">
+        <header class="flex items-center justify-between w-full p-5 bg-green-500">
+            <Link href="/">
+            <img src="../../images/logo.svg" alt="Logo" />
+            </Link>
+            <div class="flex gap-4 text-white">
+                <Link href="/">Strona główna</Link>
+                <Link href="/aktualnosci">Aktualności</Link>
+                <Link href="/menu">Menu</Link>
+                <Link href="/galeria">Galeria</Link>
+                <Link href="/kontakt">Kontakt</Link>
+            </div>
+        </header>
+        <div class="w-full flex-grow">
+            <slot />
+        </div>
+        <footer class="flex items-center justify-center w-full p-5 bg-green-500">
+            <div class="text-white">
+                © 2023 Pizzarella
+            </div>
+        </footer>
+    </div>
+</template>
---
+++ resources/js/Pages/Home.vue
@@ -0,0 +1,21 @@
+<script setup>
+import MainLayout from '@/Layouts/MainLayout.vue';
+import { Head, Link } from '@inertiajs/vue3';
+</script>
+
+<template>
+    <MainLayout>
+
+        <Head title="Strona główna" />
+
+        <div>
+            Pizza
+        </div>
+    </MainLayout>
+</template>
+
+<style>
+.abc {
+    color: red;
+}
+</style>
