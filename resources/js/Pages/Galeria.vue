6aa38f0fb7d3d59f53f9ae9230443a3339834b1c
Tobiasz-Sobczyk <tobiasz.sobczyk@student.po.edu.pl>
2023-11-23 20:27:16 +0100

    galeria zdjęć

--- resources/js/Pages/Galeria.vue
+++ resources/js/Pages/Galeria.vue
@@ -1,6 +1,16 @@
 <script setup>
 import { Head } from '@inertiajs/vue3';
 import MainLayout from '@/Layouts/MainLayout.vue';
+
+const images = () => {
+    const images = [];
+    for (let i = 0; i < 30; i++) {
+        images.push({
+            url: "https://picsum.photos/seed/picsum" + i + "/400/200",
+        });
+    }
+    return images;
+};
 </script>
 
 <template>
@@ -8,9 +18,13 @@ import MainLayout from '@/Layouts/MainLayout.vue';
 
         <Head title="Galeria" />
 
-        <div>
-            Galeria
+        <div class="flex flex-col gap-8 items-center py-10">
+            <h2 class="text-3xl">Galeria</h2>
+            <div class="flex flex-wrap justify-center gap-4">
+                <div v-for="image in images()" class="">
+                    <img :src="image.url" alt="Zdjęcie">
+                </div>
+            </div>
         </div>
-        
     </MainLayout>
 </template>
